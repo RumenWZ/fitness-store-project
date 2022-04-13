a9a1a3c556d6eae518f14d74d1f8058bc1ad79a5
RumenTashev <98177567+RumenWZ@users.noreply.github.com>
2022-04-13 18:00:23 +0300

    .env settings

--- FitnessStore/FitnessStore/settings.py
+++ FitnessStore/FitnessStore/settings.py
@@ -26,13 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w8y^+0o$zi5!n4f%cas-qa-_*f&=jzik=41q3+s^l-4qwzkw68'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')
 
-ALLOWED_HOSTS = [
-    'fitness-store-project.herokuapp.com',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
+ENVIRONMENT = os.getenv('ENVIRONMENT')
 
 # Application definition
 
@@ -86,27 +84,27 @@ WSGI_APPLICATION = 'FitnessStore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'fitness_store_dbx',
-#         'USER': 'postgres',
-#         'PASSWORD': '12345zxc',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dej179qenjvp9v',
-        'USER': 'dysxfulhwxtgpm',
-        'PASSWORD': '74804b231f7c8c403de53797e60daac29e194f5674de6d1652474abcf0975294',
-        'HOST': 'ec2-52-18-116-67.eu-west-1.compute.amazonaws.com',
+        'NAME': 'fitness_store_dbx',
+        'USER': 'postgres',
+        'PASSWORD': '12345zxc',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
+if ENVIRONMENT == 'Development':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'dej179qenjvp9v',
+            'USER': 'dysxfulhwxtgpm',
+            'PASSWORD': '74804b231f7c8c403de53797e60daac29e194f5674de6d1652474abcf0975294',
+            'HOST': 'ec2-52-18-116-67.eu-west-1.compute.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
